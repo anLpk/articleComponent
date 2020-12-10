2f94ad3e77d123cc2d653a2cef7cc1584ebb4e30
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-12-10 17:10:33 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const share = document.querySelector(".share");
+const shareSocial = document.querySelector(".social-mobile");
+
+share.addEventListener("click", () => {
+  if (shareSocial.style.visibility === "visible") {
+    shareSocial.style.visibility = "hidden";
+    share.style.backgroundColor = "hsl(210, 46%, 95%)";
+  } else {
+    shareSocial.style.visibility = "visible";
+    share.style.backgroundColor = "hsl(217, 19%, 35%)";
+  }
+});
